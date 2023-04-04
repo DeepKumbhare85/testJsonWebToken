21d1c0d8498b52fbe5dfd81435823c169c87b2a7
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-04-04 12:19:23 +0530

    repo: initalized

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="createToken">Create Token</button>
+    <button @click="verifyToken">Verify Token</button>
+  </div>
+</template>
+
+<script setup>
+
+import { sign, verify } from 'jsonwebtoken-esm';
+let token;
+const secret = 'my-secret'
+const createToken = () => {
+  console.log('hello')
+  token = sign({ name: "deep" }, secret, { expiresIn: '10s' })
+  console.log(token);
+}
+
+const verifyToken = () => {
+  console.log('your token is : ', token)
+  try {
+    const decoded = verify(token, secret)
+    console.log("Your decoded data: ", decoded);
+  }
+  catch {
+    console.error('token expired try to create new token')
+  }
+}
+
+</script>
